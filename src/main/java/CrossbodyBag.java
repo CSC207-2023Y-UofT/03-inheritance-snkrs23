14345d968cf0fd76e1f7d26adb4c86cdd3d5816d
snkrs23 <80235486+snkrs23@users.noreply.github.com>
2023-06-07 12:21:25 -0400

    Update CrossbodyBag.java

--- src/main/java/CrossbodyBag.java
+++ src/main/java/CrossbodyBag.java
@@ -7,7 +7,7 @@
  */
 
 
-    public class CrossbodyBag extends Bag {
+public class CrossbodyBag extends Bag {
         public int numberOfStraps;
         public CrossbodyBag(String color, int capacity, int numberOfStraps){
             super(color, capacity);
@@ -21,7 +21,7 @@
         public void enhance() {
             super.increaseCapacity(2);
         }
-        
+
         @Override
         public String toString() {
             return (this.getColor() + " Crossbody Bag (" + this.getNumberOfStraps() + " straps " + this.getNumberOfContents() +
