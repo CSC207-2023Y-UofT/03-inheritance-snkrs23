73f2a00b5a37b5b97d9d38e6486a6e32b50699ee
snkrs23 <80235486+snkrs23@users.noreply.github.com>
2023-06-07 11:41:00 -0400

    Update HandBag.java

--- src/main/java/HandBag.java
+++ src/main/java/HandBag.java
@@ -1,11 +1,3 @@
-/**
- * This file defines an child class named HandBag.
- * We have provided most of the code for this class, however,
- * you should implement the provided TODOs.
- *
- * You may find the readings in chapter 3. Relationships between Classes
- * helpful while working through this exercise.
- */
 public class HandBag extends Bag{
 
     /**
