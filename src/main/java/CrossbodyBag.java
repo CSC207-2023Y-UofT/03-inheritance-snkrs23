a6a3d76115449013ce9d0f2c7fc59d0d79d740f5
snkrs23 <80235486+snkrs23@users.noreply.github.com>
2023-06-07 14:22:09 -0400

    Update CrossbodyBag.java

--- src/main/java/CrossbodyBag.java
+++ src/main/java/CrossbodyBag.java
@@ -1,21 +1,53 @@
+/*
+ * This file is currently empty, but with a variety of TODOs to implement.
+ * You may want to refer to HandBag.java to see how to structure your code.
+ *
+ * You may find the readings in chapter 3. Relationships between Classes
+ * helpful while working through this exercise.
+ */
+
+/*
+ * TODO: Create a public class named CrossbodyBag which is a subclass of Bag
+ *       In addition to the attributes in Bag, the CrossbodyBag should have an
+ *       attribute named "numberOfStraps".
+ *
+ *       The constructor for CrossbodyBag should take the following parameters
+ *       in order:
+ *           1. a String for the color
+ *           2. an int for the capacity
+ *           3. an int for the number of straps
+ *
+ *       CrossbodyBag should also have a getter method called getNumberOfStraps
+ *       and it should implement the enhance method.
+ *
+ *       The enhance method should increase the capacity of the CrossbodyBag by
+ *       2 (as opposed to the 1 that HandBag increases by).
+ *
+ *       The toString method should be overridden to return a string in the
+ *       in the same form as Bag's toString but with the original "Bag" part
+ *       of the string replaced by:
+ *           Crossbody Bag with {numberOfStraps} straps
+ *
+ *       See the tests in test/CrossBodyBagTest.java for examples.
+ */
 public class CrossbodyBag extends Bag {
-        public int numberOfStraps;
-        public CrossbodyBag(String color, int capacity, int numberOfStraps){
-            super(color, capacity);
-            this.numberOfStraps = numberOfStraps;
-        }
-        public int getNumberOfStraps(){
-            return this.numberOfStraps;
-        }
+    public int numberOfStraps;
+    public CrossbodyBag(String color, int capacity, int numberOfStraps){
+        super(color, capacity);
+        this.numberOfStraps = numberOfStraps;
+    }
+    public int getNumberOfStraps(){
+        return this.numberOfStraps;
+    }
 
-        @Override
-        public void enhance() {
-            super.increaseCapacity(2);
-        }
+    @Override
+    public void enhance() {
+        super.increaseCapacity(2);
+    }
 
-        @Override
-        public String toString() {
-            return super.getColor() + " Crossbody Bag (" + this.getNumberOfStraps() + " straps " + super.getNumberOfContents() +
+    @Override
+    public String toString() {
+        return super.getColor() + " Crossbody Bag (" + this.getNumberOfStraps() + " straps " + super.getNumberOfContents() +
                 " " + super.getCapacity() + ")";
-        }
+    }
 }
