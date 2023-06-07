5424979fbf4302d335e67d62251cf13e2fd7a9e8
snkrs23 <80235486+snkrs23@users.noreply.github.com>
2023-06-07 14:25:26 -0400

    Update BagMain.java

--- src/main/java/BagMain.java
+++ src/main/java/BagMain.java
@@ -1,3 +1,10 @@
+/* This file contains a few exercises and TODOs for you to fill.
+ * Make sure you do the TODOs in Bag.java, HandBag.java and CrossbodyBag.java
+ * as the tasks in this file depends on the completion on those!
+ *
+ * You may find the readings in chapter 3. Relationships between Classes
+ * helpful while working through this exercise.
+ */
 class BagMain {
     /**
      * TODO: Implement this method
@@ -8,7 +15,6 @@ class BagMain {
      * on new Bag types (and HandBag subclasses)!
      */
     public static void enhanceBags(Bag[] bags, boolean double_enhance_handbags) {
-        // TODO: Implement this.
         for (Bag bag: bags){
             bag.enhance();
             if (double_enhance_handbags && bag instanceof HandBag) {
@@ -16,7 +22,6 @@ class BagMain {
             }
         }
     }
-
 
     /**
      * TODO: Implement this method
@@ -28,7 +33,6 @@ class BagMain {
      * @return The total number of straps of CrossbodyBags.
      */
     public static int countCrossbodyStraps(Bag[] bags) {
-        // TODO: Implement this.
         int totalNumberstraps = 0;
         for (Bag bag: bags){
             if (bag instanceof CrossbodyBag){
