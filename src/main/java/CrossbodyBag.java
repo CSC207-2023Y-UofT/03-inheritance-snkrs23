e1770733f10edec2d15f5477e2adf8b40a34ddce
snkrs23 <80235486+snkrs23@users.noreply.github.com>
2023-06-10 00:47:22 -0400

    Update CrossbodyBag.java

--- src/main/java/CrossbodyBag.java
+++ src/main/java/CrossbodyBag.java
@@ -1,7 +1,7 @@
 /*
  * This file is currently empty, but with a variety of TODOs to implement.
  * You may want to refer to HandBag.java to see how to structure your code.
- *
+ * 
  * You may find the readings in chapter 3. Relationships between Classes
  * helpful while working through this exercise.
  */
