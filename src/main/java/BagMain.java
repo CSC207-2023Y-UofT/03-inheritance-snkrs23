1873db6316e06766b0deef06dda108f849dc56b8
snkrs23 <80235486+snkrs23@users.noreply.github.com>
2023-06-10 00:46:11 -0400

    Update BagMain.java

--- src/main/java/BagMain.java
+++ src/main/java/BagMain.java
@@ -1,5 +1,5 @@
 /* This file contains a few exercises and TODOs for you to fill.
- * Make sure you do the TODOs in Bag.java, HandBag.java and CrossbodyBag.java
+ * Make sure you do the TODOs in Bag.java, HandBag.java and CrossbodyBag.java 
  * as the tasks in this file depends on the completion on those!
  *
  * You may find the readings in chapter 3. Relationships between Classes
